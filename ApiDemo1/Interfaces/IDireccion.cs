ff7ec8ed28239c30a0988c2e01f043701eea96ad
vidapogosoft <vidapogosoft@hotmail.com>
2022-08-02 19:18:58 -0500

    api del ejercicio demo 1

---
+++ ApiDemo1/Interfaces/IDireccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ApiDemo1.Modelos.Database;
+
+namespace ApiDemo1.Interfaces
+{
+    public interface IDireccion
+    {
+        void InsertDireccion(Direcciones New);
+
+        IEnumerable<Direcciones> GetDireccionesByIdRegistrado(string IdRegistrado);
+    }
+}
