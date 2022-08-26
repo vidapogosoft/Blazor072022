22dd1eb9266717f5a8f2f4d69f64976d1fac0a18
vidapogosoft <vidapogosoft@hotmail.com>
2022-08-25 20:19:35 -0500

    Taller fileupload de images

---
+++ BLUploadImages/Server/Controllers/UploadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using BLUploadImages.Shared;
+
+namespace BLUploadImages.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+
+        private readonly IWebHostEnvironment env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] ImageFile[] files)
+        {
+            foreach (var file in files)
+            {
+                var buf = Convert.FromBase64String(file.base64data);
+
+                await System.IO.File.WriteAllBytesAsync(env.ContentRootPath + System.IO.Path.DirectorySeparatorChar + Guid.NewGuid().ToString("N") + "-" + file.fileName, buf);
+            }
+        }
+    }
+}
---
+++ BLUploadImages/Shared/ImageFile.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace BLUploadImages.Shared
+{
+    public class ImageFile
+    {
+        public string base64data { get; set; }
+        public string contentType { get; set; }
+        public string fileName { get; set; }
+
+    }
+}
