30b44070bbf6c199c1dc904376f2347b64768c02
vidapogosoft <vidapogosoft@hotmail.com>
2022-09-06 21:35:43 -0500

    asp-action in form

---
+++ UIidentity/Model/Registrado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace UIidentity.Model
+{
+    public class Registrado
+    {
+        public int IdRegistrado { get; set; }
+
+        [Column(TypeName = "varchar(100)"), Required(ErrorMessage = "El campo {0} no puede estar vacío")]
+        public string Nombres { get; set; }
+
+        [Column(TypeName = "varchar(100)"), Required(ErrorMessage = "El campo {0} no puede estar vacío")]
+        public string Apellidos { get; set; }
+
+        [Column(TypeName = "varchar(100)"), Required(ErrorMessage = "El campo {0} no puede estar vacío")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "varchar(10)"), Required(ErrorMessage = "El campo {0} no puede estar vacío")]
+        public string Contrasena { get; set; }
+    }
+}
